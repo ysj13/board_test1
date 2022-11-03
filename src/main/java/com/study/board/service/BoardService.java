ae8af2fb0a3ae70f027012a485e67a6a7e35a2c6
sj <ysj13@kakao.com>
2022-11-03 18:25:22 +0900

    board test

--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -3,7 +3,6 @@ package com.study.board.service;
 import com.study.board.entity.Board;
 import com.study.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
