c153a1f8db9fcb44d49686bc51f1ea9170356ea1
sj <ysj13@kakao.com>
2022-11-03 17:34:19 +0900

    board test

---
+++ src/main/java/com/study/board/service/BoardService.java
@@ -0,0 +1,39 @@
+package com.study.board.service;
+
+import com.study.board.entity.Board;
+import com.study.board.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+
+//    @Autowired
+//    private BoardRepository boardRepository;
+
+    private final BoardRepository boardRepository;
+
+    public void write(Board board) {
+
+        boardRepository.save(board);
+    }
+
+    public List<Board> boardList() {
+
+        return boardRepository.findAll();
+    }
+
+    public Board boardview(Integer id) {
+
+        return boardRepository.findById(id).get();
+    }
+
+    public void boardDelete(Integer id) {
+
+        boardRepository.deleteById(id);
+    }
+}
